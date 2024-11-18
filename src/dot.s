e223dd758626658cba648810fe48a5203a3567aa
handsomesDavid11 <p76131254@gs.ncku.edu.tw>
2024-11-19 00:44:32 +0800

    Update dot.s

--- src/dot.s
+++ src/dot.s
@@ -32,70 +32,59 @@ dot:
     blt a4, t0, error_terminate  
 
     li t0, 0            
-    li t1, 0  
-
-    addi sp, sp , -28
+    li t1, 0
+    addi sp, sp, -8
     sw s0, 0(sp)
     sw s1, 4(sp)
-    sw s2, 8(sp)
-    sw s3, 12(sp)
-    sw s4, 16(sp)
-    sw s5, 20(sp)
-    mv s0, a0
-    mv s1, a1
-
-    li t6, 4
-    li s4, 0
-    li s5, 0  
     
 
 
 loop_start:
     bge t1, a2, loop_end
     # TODO: Add your own implementation
-    lw t2, 0(s0)
-    lw t3, 0(s1)
-    li t4, 0
-mul1:
-    bge t4, t3, next
-    add t0, t0, t2
-    addi t4, t4, 1
-    j mul1
-next:
-#s4 s5 = index
-    addi t1, t1, 1
-    add s4, s4, a3
-    add s5, s5, a4
+    lw t2, 0(a0)
+    lw t3, 0(a1)
+
     li t5, 0
-    li s2, 0
-    li s3, 0
-
-addr:    #*4
-    bge t5 , t6, next2
-    addi t5, t5, 1
-    add s2 , s2, s4
-    add s3 , s3, s5
-    j addr
-
-
-next2:
-    add s0, a0, s2
-    add s1, a1, s3
+    # detect negative
+    slt s0, t2, x0
+    slt s1, t3, x0
+    beq s0, x0, pos1
+    # t2 = -t2
+    sub t2, x0, t2
+pos1: 
+    beq s1, x0, mul_loop
+    # t3 = -t3
+    sub t3, x0, t3
+mul_loop:
+    # Booth's Algorithm
+    beq t3, x0, mul_end
+    andi t4, t3, 1
+    beq t4, x0, mul_cont
+    add t5, t5, t2
+mul_cont:
+    slli t2, t2, 1
+    srli t3, t3, 1
+    j mul_loop
+mul_end:
+    beq s0, s1, mul_finish
+    sub t5, x0, t5
+mul_finish:
+    add t0, t0, t5
+    addi t1, t1, 1
+    slli t4, a3, 2
+    slli t5, a4, 2
+    add a0, a0, t4
+    add a1, a1, t5
+    j loop_start
     
-
-    j loop_start 
-
 
 loop_end:
-    mv a0, t0
     lw s0, 0(sp)
     lw s1, 4(sp)
-    lw s2, 8(sp)
-    lw s3, 12(sp)
-    lw s4, 16(sp)
-    lw s5, 20(sp)
-    addi sp, sp , 28
-    
+    addi sp, sp, 8
+
+    mv a0, t0
     jr ra
 
 error_terminate:
